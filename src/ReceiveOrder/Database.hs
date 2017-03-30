82d50031845d07912d369bf61762772cf1bf388b
Diogo Biazus <diogob@nulogy.com>
2017-03-30 16:22:23 -0400

    Add some haddock comments

--- src/ReceiveOrder/Database.hs
+++ src/ReceiveOrder/Database.hs
@@ -1,9 +1,21 @@
+{-|
+Module      : ReceiveOrder.Database
+Description : Database manipulation.
+This module provides functions that, given a database connection pool,
+manipulate database tables.
+-}
+
 module ReceiveOrder.Database where
 
 import Data.Text
 import qualified Data.Map as M
 import Hasql.Pool
 import ReceiveOrder.Domain
+
+{-
+  Given a database connection pool returns a function that
+  That converts ReceiveOrderAttributes to a Map of persisted ReceiveOrder (or a Map of errors for receive orders)
+-}
 
 createReceiveOrders :: Pool -> ByCid ReceiveOrderAttributes -> IO (Either (ByCid ReceiveOrderErrors) (ByCid ReceiveOrder))
 createReceiveOrders _  = return . tupleToEither . M.mapEither buildReceiveOrder
--- src/ReceiveOrder/Handlers.hs
+++ src/ReceiveOrder/Handlers.hs
@@ -1,3 +1,10 @@
+{-|
+Module      : ReceiveOrder.Handlers
+Description : API Handlers.
+This module provides functions to convert the results from the domain to a type that the API can interact with.
+It should contain most knowledge about HTTP types, aeson and servant.
+Knowledge about domain and database stuf comes from other modules.
+-}
 module ReceiveOrder.Handlers
   ( massCreate
   , ReceiveOrdersRequest(..)
@@ -10,6 +17,10 @@ import Network.HTTP.Types
 import Data.Aeson (encode)
 import Servant
 
+{-
+  It takes a function that inserts the ReceiveOrders in the database
+  and returns a function that Handles POST receive_orders
+-}
 massCreate :: (ByCid ReceiveOrderAttributes -> IO (Either (ByCid ReceiveOrderErrors) (ByCid ReceiveOrder)))
   -> ReceiveOrdersRequest
   -> Handler (ByCid ReceiveOrder)
